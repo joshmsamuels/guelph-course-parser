78af8e88e4cf3eecb883dc265b5b0838f21d0b75
Josh Samuels <joshsamuels04@gmail.com>
2021-02-04 16:05:41 -0500

    Normalized keys by removing spaces, slashes and conjunctions and customized several fields (lecture/lab/exam, course id, available/capacity)

--- src/parsers/webadvisor.js
+++ src/parsers/webadvisor.js
@@ -33,12 +33,15 @@ class HeaderParser {
     constructor() {
         this.headers = []   
         this.tmpText = ""
+        
+        // remove spaces, slashes and "and" to normalize the JSON
+        this.removeRegex = /[ \/]|and/g
     }
   
     text(text) {
         this.tmpText += text.text
         if (text.lastInTextNode) {
-            this.headers.push(this.tmpText)
+            this.headers.push(this.tmpText.replace(this.removeRegex, ''))
             this.tmpText = ""
         }
     }
@@ -57,10 +60,57 @@ class BodyParser {
         this.tmpText += text.text
 
         if (text.lastInTextNode) {
-            this.tmpText = this.tmpText.trim()
+            const completeElement = this.tmpText.trim()
+            const header = this.headers[this.el % this.headers.length]
 
-            this.tmpBody[this.headers[this.el % this.headers.length]] = this.tmpText
+            switch(header) {
+                case 'Meeting Information':
+                    completeElement.split('\n').forEach((row) => {
+                        if (row.includes('LEC')) { 
+                            this.tmpBody['Lecture'] = row
+                        }
+
+                        if (row.includes('SEM')) { 
+                            this.tmpBody['Seminar'] = row
+                        }
+
+                        if (row.includes('EXAM')) { 
+                            this.tmpBody['Exam'] = row
+                        }
+                    })
+                    break
+                case 'AvailableCapacity':
+                    let availableCapacity = completeElement.split('/')
+
+                    this.tmpBody['Available'] = Number(availableCapacity[0])
+                    this.tmpBody['Capacity'] = Number(availableCapacity[1])
+                    break
+                case 'SectionNameTitle':
+                    let courseCode = completeElement.match(/[A-Z]{2,4}[*][\d]{4}[*][A-Z0-9]{2,}/)[0]
+
+                    this.tmpBody['CourseCode'] = courseCode
+                    this.tmpBody[header] = completeElement
+                    break
+                case 'Credits':
+                    let creditNum = Number(completeElement)
+                    this.tmpBody[header] = creditNum !== 'NaN' ? creditNum : completeElement
+
+                    if (creditNum === 'NaN') {
+                        console.warn(`The credit "${completeElement}" could not be parsed into a number`)
+                        this.tmpBody[header] = completeElement
+                        break;
+                    }
+
+                    this.tmpBody[header] = creditNum
+                    break;
+                default:
+                    // No additional processing on the element is needed
+                    this.tmpBody[header] = completeElement
+                    break
+            }
+
             this.tmpText = ""
+
 
             if ((this.el + 1) % this.headers.length === 0) {
                 this.body.push(this.tmpBody)
